8c56343cfa3751fe46fe9687981f16585923dd2d
Denis Sheikherev <denis.sheikherev@quantumsoft.ru>
2025-08-29 18:03:03 +0700

    fix: set  app tokens to proper apps on app.bugsee.com
    Change-Id: I5593c1b5ae931a687541ce29a140feada0e24e95

--- composeApp/src/androidMain/kotlin/com/bugsee/kmp/sample/Platform.android.kt
+++ composeApp/src/androidMain/kotlin/com/bugsee/kmp/sample/Platform.android.kt
@@ -5,7 +5,7 @@ import android.os.Build
 class AndroidPlatform : Platform {
     override val name: String = "Android ${Build.VERSION.SDK_INT}"
 
-    override val token: String = "431f6313-07b5-47e5-b283-adb8982f867a"
+    override val token: String = "323a506c-3756-49dd-9833-39fef912639c"
 }
 
 actual fun getPlatform(): Platform = AndroidPlatform()
--- composeApp/src/iosMain/kotlin/com/bugsee/kmp/sample/Platform.ios.kt
+++ composeApp/src/iosMain/kotlin/com/bugsee/kmp/sample/Platform.ios.kt
@@ -5,7 +5,7 @@ import platform.UIKit.UIDevice
 class IOSPlatform: Platform {
     override val name: String = UIDevice.currentDevice.systemName() + " " + UIDevice.currentDevice.systemVersion
 
-    override val token: String = "377bb874-6ec8-4a70-8028-63e6972cedc6"
+    override val token: String = "a710ea5f-a2bc-4486-b84a-bf1bfcc79b12"
 }
 
 actual fun getPlatform(): Platform = IOSPlatform()
